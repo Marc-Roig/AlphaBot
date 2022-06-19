d09de89eb6e8ee3fd2bcd63edd6c30b0de0b1048
Marc <marc12info@gmail.com>
2022-06-19 19:48:51 +0200

    🐛 Fix typo

--- src/commands/jobs/book_scheduled.py
+++ src/commands/jobs/book_scheduled.py
@@ -3,12 +3,12 @@ Cron to book scheduled classes.
 """
 from telegram.ext import CallbackContext
 from src.use_cases.book_user_scheduled_classes import make_bookings
-from src.infrastructure import user_mongo_repository
+from src.infrastructure import user_repository
 import asyncio
 
 async def handler(context: CallbackContext) -> None:
     
-    users = await user_mongo_repository.get_all()
+    users = await user_repository.get_all()
     bookings_promises = []
 
     for user in users:
