c69d7d4a0429cca0d5b3a2d45af7b115fb7859e1
Marc <marc12info@gmail.com>
2022-07-02 10:50:55 +0200

    🐛 Display current month in calendar

--- src/commands/user/make_bookings/display_calendar.py
+++ src/commands/user/make_bookings/display_calendar.py
@@ -5,7 +5,7 @@ from telegram import Update
 from telegram.ext import CallbackContext
 from src.utils.calendar import create_calendar
 from src.utils import decorators
-
+import datetime
 
 @decorators.user
 async def handler(update: Update, context: CallbackContext) -> None:
@@ -18,6 +18,8 @@ async def handler(update: Update, context: CallbackContext) -> None:
     if query:
         await query.answer()
     
+    now = datetime.datetime.now()
+
     await bot.send_message(update.effective_message.chat_id,
                      f"Select Day",
-                     reply_markup=create_calendar(2022, 6))
+                     reply_markup=create_calendar(now.year, now.month))
